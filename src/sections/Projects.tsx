3886d2be7ce1620555d5ef216b3455130e3fcf75
mrckie <marckie.js@gmail.com>
2025-06-12 21:30:23 +0800

    Project.tsx section was already done in the previous development
    Problem: Encounted a problem regarding size limit of github which is 50mb. I put a video over 200mb in the assets
    
    Solution: Host the video externally

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,9 +1,101 @@
+import CustomCard from "@/components/ui/custom-card"
 
 const Projects = () => {
 	return (
-		<section>
-			<h1 className='text-xl font-semibold lg:text-2xl lg:font-bold'>Specialized Projects</h1>
+		<section className="flex flex-col items-start gap-y-18 lg:gap-y-23">
+			<h1 className='text-xl font-semibold md:text-4xl md:font-bold'>Specialized Projects</h1>
+			<div className="flex flex-col gap-y-30">
+				<div className="flex flex-col gap-y-8 items-start justify-between lg:grid lg:grid-cols-2">
+					<video
+						autoPlay
+						loop
+						muted
+						className="rounded-2xl shadow-lg duration-300 ease-in-out hover:-translate-y-1 hover:scale-102">
+						<source src='https://res.cloudinary.com/drotczcme/video/upload/q_auto/v1749723270/a0djjbgwboaombl52e9l.mp4' type="video/mp4" />
+					</video>
+					<CustomCard
+						title="Barangay Document Requesting System"
+						description="Designed to simplify and digitize the process of requesting official barangay documents."
+						features={[
+							'Authentication, password reset via email, change password, and profile management',
+							'Resident verification and administrator invitation',
+							'Role based access control',
+							'Promote/Demote Role',
+							'Real-time tracking',
+							'Notification system'
+						]}
+						techStacks={[
+							'Tailwind CSS',
+							'Laravel',
+							'React Typescript',
+							'Inertia.js',
+							'MariaDB',
+							'Vite'
+						]}
+						link="https://github.com/mrckie/brgydocreq-system"
+					/>
 
+				</div>
+
+				<div className="flex flex-col gap-y-8 items-start justify-between lg:grid lg:grid-cols-2">
+					<video
+						autoPlay
+						loop
+						muted
+						className="rounded-2xl duration-300 ease-in-out hover:-translate-y-1 hover:scale-102">
+						<source src='https://res.cloudinary.com/drotczcme/video/upload/q_auto/v1749723509/b2kakrkdmyrhatbnfqwj.mp4' type="video/mp4" />
+					</video>
+					<CustomCard
+						title="To-do App"
+						description="A simple to-do app that can perform CRUD"
+						features={[
+							'CRUD',
+							'Field validation',
+							'Marking when is done',
+						]}
+						techStacks={[
+							'Tailwind CSS',
+							'Laravel',
+							'React Javascript',
+							'Inertia.js',
+							'MariaDB',
+							'Vite',
+						]}
+						link="https://github.com/mrckie/todo-app"
+					/>
+
+				</div>
+
+				<div className="flex flex-col gap-y-8 items-start justify-between lg:grid lg:grid-cols-2">
+					<video
+						autoPlay
+						loop
+						muted
+						className="rounded-2xl shadow-lg duration-300 ease-in-out hover:-translate-y-1 hover:scale-102">
+						<source src='https://res.cloudinary.com/drotczcme/video/upload/q_auto/v1749726631/3rd_project_dfuvnl.mp4' type="video/mp4" />
+					</video>
+					<CustomCard
+						title="Blog Website"
+						description="A simple blog website for publishing and reading articles, with a clean design and basic content management features."
+						features={[
+							'CRUD',
+							'Authentication for admin and visitor access',
+							'Marking when is done',
+							'Comment section for reader interaction',
+							'Admin dashboard for creating, editing, and deleting posts',
+							'Basic category or tag filtering'
+						]}
+						techStacks={[
+							'Vanilla Php',
+							'CSS',
+							'HTML',
+							'Vanilla Javascript',
+							'MySQL',
+						]}
+						link="https://github.com/mrckie/uhem"
+					/>
+				</div>
+			</div>
 		</section>
 
 	)
