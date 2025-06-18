d64e6c97092a3246753a5e23ef5d1e61e8c90ee3
mrckie <marckie.js@gmail.com>
2025-06-18 20:30:57 +0800

    Add lenis library for smooth scrolling; implement CanvasCursor component and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+"use client";
 import './index.css'
 import NavHeader from './components/ui/nav-header'
 import { ThemeProvider } from './components/theme-provider'
@@ -7,21 +8,27 @@ import AboutMe from './sections/AboutMe'
 import Projects from './sections/Projects'
 import Footer from './sections/Footer'
 import { SplashScreen } from './components/magicui/splash-screen'
-
+import { ReactLenis } from 'lenis/react'
+import CanvasCursor from './components/magicui/canvas-cursor';
 
 function App() {
+
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <NavHeader />
-      <SplashScreen />
-      <main className='relative px-8 lg:px-10 z-0 flex flex-col gap-y-30 py-30 lg:py-53 md:gap-y-53'>
-        <Hero />
-        <AboutMe />
-        <Projects />
-        <TechnicalSkills />
-        <Footer />
-      </main>
-    </ThemeProvider>
+    <>
+      <CanvasCursor />
+      <ReactLenis root />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <NavHeader />
+        <SplashScreen />
+        <main className='relative px-8 lg:px-10 z-0 flex flex-col gap-y-30 py-30 lg:py-53 md:gap-y-53'>
+          <Hero />
+          <AboutMe />
+          <Projects />
+          <TechnicalSkills />
+          <Footer />
+        </main>
+      </ThemeProvider>
+    </>
   )
 }
 
